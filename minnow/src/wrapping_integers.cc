339bfd20c822f26fa84b9d7e02dba20c0593bc39
ccjincong <chenjincong11@outlook.com>
2023-07-15 09:42:34 +0000

    lab2 test1

--- minnow/src/wrapping_integers.cc
+++ minnow/src/wrapping_integers.cc
@@ -1,19 +1,27 @@
 #include "wrapping_integers.hh"
+#include <fstream>
+#include <iostream>
 
 using namespace std;
 
-Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
-{
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+Wrap32 Wrap32::wrap (uint64_t n , Wrap32 zero_point) {
+
+    return Wrap32{ (zero_point + (uint32_t) n) };
 }
 
-uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
-{
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+uint64_t Wrap32::unwrap (Wrap32 zero_point , uint64_t checkpoint) const {
+    // Your code here.
+    uint32_t len = raw_value_ - zero_point.raw_value_;
+    uint32_t checkpoint_32 = checkpoint;  // 直接截取后面的
+
+    uint64_t seno = checkpoint - checkpoint_32 + len;
+
+    if ( checkpoint_32 > len && checkpoint_32 - len > uint32_t (1 << 31))
+        seno += (1ull << 32);
+
+    if ( checkpoint_32 < len && len - checkpoint_32 > uint32_t (1 << 31) && seno > (1ull << 32))
+        seno -= (1ull << 32);
+
+    return seno;
+
 }
